4ec15e15417ba0de32ac2b774c17bdd7707468be
ASMA <97527150+asma-raaishu@users.noreply.github.com>
2022-01-19 19:07:42 +0530

    Add files via upload

---
+++ ASSIGNMENT5(18.01.2022)/Natural_no.java
@@ -0,0 +1,13 @@
+public class Natural_no
+{
+public static void main(String args[])
+{
+int i;
+System.out.println("the first 10 natural numbers are:\n");
+for(i=0;i<=10;i++)
+{
+System.out.println(i);
+}
+System.out.println("\n");
+}
+}
---
+++ ASSIGNMENT5(18.01.2022)/Pattern.java
@@ -0,0 +1,25 @@
+public class Pattern
+{
+public static void main(String args[])
+{
+int i=1;
+while(i==1)
+{
+System.out.println(i);
+i++;
+}
+int j=111;
+while(j==111)
+{
+	System.out.println(j);
+	j++;
+}
+int k=11111;
+while(k==11111)
+{
+	System.out.println(k);
+	k++;
+}
+
+}
+}
