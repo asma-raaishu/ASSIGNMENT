51da83a4abf23b0c524e535c73c9bd997871f07e
ASMA <97527150+asma-raaishu@users.noreply.github.com>
2022-01-19 19:07:04 +0530

    Add files via upload

---
+++ ASSIGNMENT4(17.01.2022)/Sign_unsign.java
@@ -0,0 +1,17 @@
+class Sign_unsign
+{
+public static void main(String args[])
+{
+int sign=2345;
+int unsign=-2346;
+if(sign==unsign)
+{
+System.out.println("the signed number is :"+ sign);
+}
+else
+{
+System.out.println("the unsigned number is :"+ unsign);
+}
+System.out.println(sign+unsign);
+}
+}
