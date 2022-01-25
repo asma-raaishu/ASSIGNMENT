19a4d0f1d342611302d4b00ac93360682c0fb081
ASMA <97527150+asma-raaishu@users.noreply.github.com>
2022-01-25 16:02:26 +0530

    Add files via upload

---
+++ ASSIGNMENT6(20.01.2022)/Student.java
@@ -0,0 +1,13 @@
+
+public class Student {
+int roll_no;
+String name;
+public static void main(String args[])
+{
+	Student s=new Student();
+	s.name="john";
+	s.roll_no=2;
+	System.out.println("Name is: "+s.name);
+	System.out.println("roll_no: "+s.roll_no);
+}
+}
