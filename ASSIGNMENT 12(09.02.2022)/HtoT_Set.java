85c99a2e5389cf5e4ba1410fa1aabbc219116f27
ASMA <97527150+asma-raaishu@users.noreply.github.com>
2022-02-14 17:56:20 +0530

    Add files via upload

---
+++ ASSIGNMENT 12(09.02.2022)/HtoT_Set.java
@@ -0,0 +1,28 @@
+package collection;
+import java.util.*;
+public class HtoT_Set
+{
+	public static void main(String[] args)
+	{
+		 HashSet<String> h_set = new HashSet<String>();
+		                                       // use add() method to add values in the hash set
+		          h_set.add("Red");
+		          h_set.add("Green");
+		          h_set.add("Black");
+		          h_set.add("White");
+		          h_set.add("Pink");
+		          h_set.add("Yellow");
+		      System.out.println("Original Hash Set: " + h_set);
+		    
+		                                       // Create a TreeSet of HashSet elements
+		     Set<String> tree_set = new TreeSet<String>(h_set);
+		 
+		                                       // Display TreeSet elements
+		     System.out.println("TreeSet elements: ");
+		     for(String element : tree_set){
+		        System.out.println(element);
+		     }
+
+	}
+
+}
