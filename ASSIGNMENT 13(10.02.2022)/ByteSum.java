b0f8c01bd8cfca13fda19198f2108465e1aa31cc
ASMA <97527150+asma-raaishu@users.noreply.github.com>
2022-02-14 18:13:54 +0530

    Add files via upload

---
+++ ASSIGNMENT 13(10.02.2022)/ByteSum.java
@@ -0,0 +1,19 @@
+package collection;
+
+public class ByteSum 
+{
+	public static void main(String[] args)
+	{
+	                               // create two variable of byte type
+	        byte firstByte = 13;
+	        byte secondByte = 123;
+	  
+	                                 // create int variable to store result
+	        int sum;
+	  
+	        sum = firstByte + secondByte;
+	        System.out.println("sum = " + sum);
+	    }
+	}
+
+
